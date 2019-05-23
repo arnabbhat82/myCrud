0602e7a13eb20d0cb8d5ceb75a7a8f77dec69a62
arnabbhat82 <arnabbhat@gmail.com>
2019-05-23 19:55:25 +0530

    read and delete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'myCrud';
+  fruits = ['Oranges', 'Apples', 'kiwi', 'banana'];
+
+  deleteItem(i: number) {
+    this.fruits.splice(i, 1);
+  }
 }
