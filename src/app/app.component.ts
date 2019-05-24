543e25556465953a232d543e05422b775fb5b1d6
Ambar Lahiri <ambarlahiri@gmail.com>
2019-05-24 19:55:42 +0530

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   fruits = ['Oranges', 'Apples', 'kiwi', 'banana'];
+  signupForm: FormGroup;
 
-  deleteItem(i: number) {
-    this.fruits.splice(i, 1);
-  }
+ deleteItem(i: number){
+   this.fruits.splice(i,1);
+ }
+ 
+ ngOnInit(){
+   this.signupForm = new FormGroup({
+     'fruitItem': new FormControl(null)
+   });
+ }
+ onSubmit(){
+  console.log(this.signupForm.value);
+}
 }
