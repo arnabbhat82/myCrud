39307a65f604d7dc01c83ebaa7f3ff27e027fdbb
Ambar Lahiri <ambarlahiri@gmail.com>
2019-05-25 17:49:52 +0530

    edit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,22 @@
-import { Component } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { Component } from "@angular/core";
+import { FormGroup, FormControl } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
   fruits: string[];
   signupForm: FormGroup;
+  editFruitIndex: number;
 
   constructor() {
-    this.fruits = ['Oranges', 'Apples', 'kiwi', 'banana'];
-    
+    this.fruits = ["Oranges", "Apples", "kiwi", "banana"];
     this.signupForm = new FormGroup({
       fruitItem: new FormControl(null)
     });
+    this.editFruitIndex = -1;
   }
 
   get fruitItem() {
@@ -23,17 +24,25 @@ export class AppComponent {
   }
 
   onSubmit() {
-    console.log(this.fruitItem.value);
-    this.fruits.push(this.fruitItem.value);
+    if (this.editFruitIndex === -1) {
+      // add (push adds new element to the end of the array)
+      this.fruits.push(this.fruitItem.value);
+    } else {
+      // update
+      // this.fruits.(this.fruitItem.value);
+      this.fruits[this.editFruitIndex] = this.fruitItem.value;
+
+    }
   }
 
-  deleteItem(i: number) {
+  deleteFruitName(i: number) {
     this.fruits.splice(i, 1);
   }
 
   editFruitName(i: number) {
-    // console.log();
-    //console.log(this.fruits.valueOf());
-    console.log(this.fruits[i]);    
+    // console.log(this.fruits[i]);
+    //  return this.editfruitItemName = this.fruits[i];
+    this.editFruitIndex = i;
+    this.fruitItem.setValue(this.fruits[i]);
   }
 }
